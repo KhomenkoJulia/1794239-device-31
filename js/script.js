d53e260f289b9602e88b746fda6b4879fdda87a6
Юлька <j9151139558@gmail.com>
2021-06-27 14:41:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-// const modalLetter = document.querySelector('.modal-letter');
-// const modalMap = document.querySelector('.modal-map');
-
-// const letterButton = document.querySelector('.info-form-link');
-// const mapButton = document.querySelector('.map-link');
-
-// const modalCloseButton = document.querySelectorAll('.modal-close');
-
-// letterButton.addEventListener('click', (evt) => {
-//   evt.preventDefault();
-//   modalLetter.style.display = 'block';
-// });
-// mapButton.addEventListener('click', (evt) => {
-//   evt.preventDefault();
-//   modalMap.style.display = 'block';
-// });
-// modalCloseButton.forEach((item => {
-//   item.addEventListener('click', (evt) => {
-//     evt.preventDefault();
-//     modalLetter.style.display = 'none';
-//     modalMap.style.display = 'none';
-//   });
-// }));
-
-
 const modalLetter = document.querySelector('.modal-letter');
 const modalMap = document.querySelector('.modal-map');
 const letterButton = document.querySelector('.info-form-link');
@@ -32,6 +7,13 @@ const modalCloseButton = document.querySelectorAll('.modal-close');
 const letterForm = modalLetter.querySelector(".letter-form");
 const nameInput = modalLetter.querySelector(".letter-input-name");
 const emailInput = modalLetter.querySelector(".letter-input-email");
+
+const paginationList = document.querySelectorAll('.pagination-link');
+const linkHref = [
+  '#slide1',
+  '#slide2',
+  '#slide3'
+];
 
 let isStorageSupport = true;
 let storage = "";
@@ -64,38 +46,43 @@ modalCloseButton.forEach((item => {
 		evt.preventDefault();
     modalLetter.style.display = 'none';
     modalMap.style.display = 'none';
-    modalLetter.classList.remove("modal-error");
+    modalLetter.classList.remove('modal-error');
 	});
 }));
 
 letterForm.addEventListener("submit", function (evt) {
   if (!nameInput.value || !emailInput.value) {
     evt.preventDefault();
-    modalLetter.classList.remove("modal-error");
-    modalLetter.offsetWidth = loginPopup.offsetWidth;
-    modalLetter.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-    localStorage.setItem("name", nameInput.value);
-  }
+    modalLetter.classList.remove('modal-error');
+    modalLetter.offsetWidth = modalLetter.offsetWidth;
+    modalLetter.classList.add('modal-error');
+  } else if (isStorageSupport) {
+    localStorage.setItem('name', nameInput.value);
 }
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  if (evt.key === 'Escape') {
     if (modalLetter.style.display = 'block') {
       evt.preventDefault();
       modalLetter.style.display = 'none';
-      modalLetter.classList.remove("modal-error");
+      modalLetter.classList.remove('modal-error');
     }
   }
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  if (evt.key === 'Escape') {
     if (modalMap.style.display = 'block') {
       evt.preventDefault();
       modalMap.style.display = 'none';
     }
   }
+});
+
+paginationList.forEach((item, index)=>{
+  item.addEventListener('click', () => {
+    item.href = linkHref[index];
+    item.classList.toggle('pagination-link-active');
+  });
 });
